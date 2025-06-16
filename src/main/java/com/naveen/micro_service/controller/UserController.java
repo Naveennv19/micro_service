ec1e135eeb23369db685f38a218b509b7145fd0c
Naveennv19 <naveennv19104@gmail.com>
2025-06-16 13:02:16 +0530

    .

--- src/main/java/com/naveen/micro_service/controller/UserController.java
+++ src/main/java/com/naveen/micro_service/controller/UserController.java
@@ -1,15 +1,9 @@
 package com.naveen.micro_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.naveen.micro_service.model.User;
 import com.naveen.micro_service.repository.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 
 @RestController
